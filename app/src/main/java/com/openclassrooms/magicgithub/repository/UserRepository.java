de70265f30651a764993909c91cb26d5777ba1e1
David <davidvignon86@yahoo.fr>
2021-11-03 14:10:31 +0100

    Améliorations

--- app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -1,36 +1,26 @@
 package com.openclassrooms.magicgithub.repository;
 
-import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.FAKE_USERS_RANDOM;
 import com.openclassrooms.magicgithub.api.ApiService;
 import com.openclassrooms.magicgithub.model.User;
-
 import java.util.List;
-import java.util.Random;
 
 public class UserRepository {
 
-    private final ApiService apiService; // TODO: A utiliser
+    private final ApiService apiService;
 
     public UserRepository(ApiService apiService) {
         this.apiService = apiService;
     }
 
     public List<User> getUsers() {
-        // TODO: A modifier
         return apiService.getUsers();
-
     }
 
     public void generateRandomUser() {
-        // TODO: A modifier
         apiService.generateRandomUser();
-//        apiService.getUsers().add(FAKE_USERS_RANDOM.get(new Random().nextInt(FAKE_USERS_RANDOM.size())));
-
     }
 
     public void deleteUser(User user) {
-//        // TODO: A modifier
-//        apiService.getUsers().remove(user);
         apiService.deleteUser(user);
     }
 }
